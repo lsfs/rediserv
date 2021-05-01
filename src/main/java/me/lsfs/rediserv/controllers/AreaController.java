72e9f0bf12ca3be17112e24372d756c1c9080cbe
Luis Fernando <lufesampaio@gmail.com>
2021-05-01 03:12:24 -0400

    feat: Adicionado métodos de listar, obter por id e inserir área

--- src/main/java/me/lsfs/rediserv/controllers/AreaController.java
+++ src/main/java/me/lsfs/rediserv/controllers/AreaController.java
@@ -2,19 +2,56 @@ package me.lsfs.rediserv.controllers;
 
 import me.lsfs.rediserv.models.Area;
 
+import me.lsfs.rediserv.services.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/area")
-public class AreaController implements IController<Area, Area> {
+public class AreaController {
 
-    @Override
+    @Autowired
+    AreaService areaService;
+
+    @GetMapping("/listar")
     public ResponseEntity<List<Area>> listar() {
+        List<Area> areas = areaService.listar();
+        return ResponseEntity.ok().body(areas);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Area> inserir(
+            @RequestBody Area area,
+            UriComponentsBuilder uriComponentsBuilder) throws Exception {
+
+        Area areaSalva = areaService.inserir(area);
+        URI uri = uriComponentsBuilder.path("/areas/{id}")
+                .buildAndExpand(areaSalva.getId()).toUri();
+        return ResponseEntity.created(uri).body(areaSalva);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Area> buscar(
+            @PathVariable Long id) {
+
+        Area area = areaService.buscar(id);
+
+        return ResponseEntity.ok().body(area);
+    }
+
+    public Area alterar(Long id, Area area) {
         return null;
     }
+
+    public void apagar(Long id) {
+
+    }
+
 }
--- src/main/java/me/lsfs/rediserv/services/AreaService.java
+++ src/main/java/me/lsfs/rediserv/services/AreaService.java
@@ -1,9 +1,44 @@
 package me.lsfs.rediserv.services;
 
+import me.lsfs.rediserv.exceptions.DadosException;
+import me.lsfs.rediserv.models.Area;
+import me.lsfs.rediserv.repositories.AreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AreaService {
-    public void listar() {
+
+    @Autowired
+    AreaRepository areaRepository;
+
+    public List<Area> listar() {
+        return areaRepository.findAll();
     }
+
+    public Area inserir(Area area) throws Exception {
+        validar(area);
+        return areaRepository.save(area);
+    }
+
+    public Area buscar(Long id) {
+
+       Area area = areaRepository.findById(id)
+                .orElseThrow(() -> new DadosException("${erro.registroNaoEncontrado}"));
+
+        return area;
+    }
+
+    private void validar(Area area) throws Exception {
+        if (area.getNome().isBlank() || area.getNome().isEmpty()){
+            throw new Exception("aaaa");
+        }
+    }
+
+
+
+
 }
