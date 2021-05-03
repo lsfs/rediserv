dd48149e926161322bf78131f463415321491c2f
Luís Fernando <lufesampaio@gmail.com>
2021-05-03 03:07:50 -0400

    feat: Criada classe de exceção para regras de negócio

--- src/main/java/me/lsfs/rediserv/exceptions/NegocioException.java
+++ src/main/java/me/lsfs/rediserv/exceptions/NegocioException.java
@@ -2,4 +2,23 @@ package me.lsfs.rediserv.exceptions;
 
 public class NegocioException extends RuntimeException{
 
+    private static final long serialVersionUID = 1L;
+    private static final String MENSAGEM_PADRAO = "Dados inválidos";
+
+    /**
+     * Construtor padrão. Utiliza a mensagem padrão conforme chave <b>erro.registroNaoEncontrado</b>
+     * declarada no arquivo <b>messages.properties</b>.
+     */
+    public NegocioException() {
+        super(MENSAGEM_PADRAO);
+    }
+
+    /**
+     * Construtor sobrecarregado para receber uma mensagem de erro como parâmetro.
+     * A mensagem deve estar declarada corretamente no arquivo <b>messages.properties</b>.
+     */
+    public NegocioException(String mensagem) {
+        super(mensagem);
+    }
+
 }
