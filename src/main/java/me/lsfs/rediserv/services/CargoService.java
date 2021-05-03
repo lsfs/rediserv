70ddafdab2afdbe3f6908b03186b17520537900c
Luís Fernando <lufesampaio@gmail.com>
2021-05-03 03:09:09 -0400

    feat: finalizada classe de serviço de Cargo

---
+++ src/main/java/me/lsfs/rediserv/services/CargoService.java
@@ -0,0 +1,105 @@
+package me.lsfs.rediserv.services;
+
+import me.lsfs.rediserv.exceptions.DadosException;
+import me.lsfs.rediserv.exceptions.NegocioException;
+import me.lsfs.rediserv.models.Area;
+import me.lsfs.rediserv.models.Cargo;
+import me.lsfs.rediserv.models.dtos.CargoDTO;
+import me.lsfs.rediserv.repositories.CargoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CargoService {
+
+    CargoRepository cargoRepository;
+    ModelMapper modelMapper;
+    AreaService areaService;
+
+    @Autowired
+    public CargoService(CargoRepository cargoRepository,
+                        ModelMapper modelMapper,
+                        AreaService areaService) {
+
+        this.modelMapper = modelMapper;
+        this.cargoRepository = cargoRepository;
+        this.areaService = areaService;
+    }
+
+
+    public List<Cargo> listar() {
+        return cargoRepository.findAll();
+    }
+
+    public Cargo inserir(CargoDTO cargoDTO) {
+        Cargo cargo = converteDTO(cargoDTO);
+
+        Area areaBuscada = buscarArea(cargoDTO.getArea());
+
+        cargo.setArea(areaBuscada);
+
+        return cargoRepository.save(cargo);
+    }
+
+    public Cargo buscar(Long id){
+        Cargo cargo = cargoRepository.findById(id)
+                .orElseThrow(()-> new DadosException("Erro: Cargo não localizado"));
+
+        return cargo;
+    }
+
+    public Cargo alterar(Long id, CargoDTO cargoDTO) {
+
+        validarDTO(cargoDTO);
+        validarID(id);
+
+        Cargo cargo = buscar(id);
+
+        cargo = modelMapper.map(cargoDTO, Cargo.class);
+        Area areaBuscada = buscarArea(cargoDTO.getArea());
+        cargo.setId(id);
+        cargo.setArea(areaBuscada);
+
+        return cargo;
+
+    }
+
+    public void apagar(Long id) {
+        validarID(id);
+
+        cargoRepository.deleteById(id);
+
+    }
+
+    private void validarDTO(CargoDTO cargoDTO) {
+        if (cargoDTO.getNome().isBlank() || cargoDTO.getNome().isBlank()) {
+            throw new NegocioException("Erro:Nome de cargo inválido");
+        }
+
+        if (cargoDTO.getArea() == null) throw new NegocioException("Erro: Área inválida");
+    }
+
+    private void validarID(Long id){
+        if(!cargoRepository.existsById(id)){
+            throw new DadosException("ID inválido.");
+        }
+    }
+
+    private Cargo converteDTO(CargoDTO cargoDTO) {
+
+        Cargo cargo = modelMapper.map(cargoDTO, Cargo.class);
+
+        return cargo;
+    }
+
+    private Area buscarArea(Long idarea){
+        Area areaBuscada = areaService.buscar(idarea);
+        return areaBuscada;
+    }
+
+
+
+}
