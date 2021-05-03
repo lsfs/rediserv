9beda9ecc8112f6f9e2bcbb583f6949517d02051
Luís Fernando <lufesampaio@gmail.com>
2021-05-03 03:01:19 -0400

    feat: Criada configuração inicial do ModelMapper

---
+++ src/main/java/me/lsfs/rediserv/configurations/ModelMapperConfiguration.java
@@ -0,0 +1,20 @@
+package me.lsfs.rediserv.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        
+
+        return modelMapper;
+    }
+
+}
