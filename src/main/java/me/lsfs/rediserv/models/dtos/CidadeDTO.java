4b1a27627ce71ff43b426b2079f14fa8300416dd
Luís Fernando <lufesampaio@gmail.com>
2021-05-04 02:24:59 -0400

    feat: Criadas classes de DTO para Cidade e Instituição

---
+++ src/main/java/me/lsfs/rediserv/models/dtos/CidadeDTO.java
@@ -0,0 +1,32 @@
+package me.lsfs.rediserv.models.dtos;
+
+public class CidadeDTO {
+
+    private Long id;
+    private String nome;
+    private Long estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Long estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/me/lsfs/rediserv/models/dtos/InstituicaoDTO.java
@@ -0,0 +1,41 @@
+package me.lsfs.rediserv.models.dtos;
+
+public class InstituicaoDTO {
+
+    private Long id;
+    private String nome;
+    private String sigla;
+    private Long estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Long getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Long estado) {
+        this.estado = estado;
+    }
+}
