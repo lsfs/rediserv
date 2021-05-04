18e9518c50218acf6da77a2ea63af3b8bd8e6627
Luís Fernando <lufesampaio@gmail.com>
2021-05-04 02:25:28 -0400

    feat: Criadas interfaces de Cidade, Estado e Instituição

---
+++ src/main/java/me/lsfs/rediserv/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package me.lsfs.rediserv.repositories;
+
+import me.lsfs.rediserv.models.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
