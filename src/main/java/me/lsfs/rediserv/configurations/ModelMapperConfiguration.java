f4f21122eebf0062d54f48f45d35d3f13c6222c7
Luis Fernando <lufesampaio@gmail.com>
2021-05-22 02:13:30 -0400

    wip: Configuração para o modelmapper converter hora em string

--- src/main/java/me/lsfs/rediserv/configurations/ModelMapperConfiguration.java
+++ src/main/java/me/lsfs/rediserv/configurations/ModelMapperConfiguration.java
@@ -1,12 +1,18 @@
 package me.lsfs.rediserv.configurations;
 
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class ModelMapperConfiguration {
+
+    DateTimeFormatter diaMesAnoFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Bean
     public ModelMapper modelMapper(){
@@ -15,6 +21,44 @@ public class ModelMapperConfiguration {
         
 
         return modelMapper;
+    }
+
+    /**
+     * "Ensina" o ModelMapper a converter objetos do tipo {@link String} para {@link LocalDate}.
+     *
+     * @return AbstractConverter<String, LocalDate> conversor configurado
+     */
+    private AbstractConverter<String, LocalDate> getStringToLocalDateConverter() {
+        return new AbstractConverter<String, LocalDate>() {
+
+            @Override
+            protected LocalDate convert(String source) {
+                if (source == null) {
+                    return null;
+                }
+
+                return LocalDate.parse(source, diaMesAnoFormatter);
+            }
+        };
+    }
+
+    /**
+     * "Ensina" o ModelMapper a converter objetos do tipo {@link LocalDate} para {@link String}.
+     *
+     * @return AbstractConverter<LocalDate, String> conversor configurado
+     */
+    private AbstractConverter<LocalDate, String> getLocalDateToStringConverter() {
+        return new AbstractConverter<LocalDate, String>() {
+
+            @Override
+            protected String convert(LocalDate source) {
+                if (source == null) {
+                    return null;
+                }
+
+                return diaMesAnoFormatter.format(source);
+            }
+        };
     }
 
 }
