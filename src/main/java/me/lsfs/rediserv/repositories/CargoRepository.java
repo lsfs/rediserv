e3f39f1de56af3c5d2226341c8de05a5dd91c3a4
Luis Fernando <lufesampaio@gmail.com>
2021-05-22 02:24:54 -0400

    feat: adição de query para obtenção de cargo por área

--- src/main/java/me/lsfs/rediserv/repositories/CargoRepository.java
+++ src/main/java/me/lsfs/rediserv/repositories/CargoRepository.java
@@ -2,8 +2,19 @@ package me.lsfs.rediserv.repositories;
 
 import me.lsfs.rediserv.models.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface CargoRepository extends JpaRepository<Cargo,Long> {
+
+
+    @Query("SELECT c FROM Cargo c "
+            + " WHERE c.area.id = :idArea ORDER BY c.nome")
+    Optional<List<Cargo>> findByArea(Long idArea);
+
 }
