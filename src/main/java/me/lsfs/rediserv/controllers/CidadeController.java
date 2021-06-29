119837d4c44df4cffe57b3c639cc9cb045a9be7d
Luís Fernando <lufesampaio@gmail.com>
2021-06-28 22:42:27 -0400

    feat: adição da busca por estado e listagem paginada

--- src/main/java/me/lsfs/rediserv/controllers/CidadeController.java
+++ src/main/java/me/lsfs/rediserv/controllers/CidadeController.java
@@ -1,9 +1,14 @@
 package me.lsfs.rediserv.controllers;
 
 import me.lsfs.rediserv.models.Cidade;
-import me.lsfs.rediserv.models.dtos.CidadeSaveDTO;
+import me.lsfs.rediserv.dtos.CidadeSaveDTO;
+import me.lsfs.rediserv.models.Estado;
+import me.lsfs.rediserv.models.Instituicao;
 import me.lsfs.rediserv.services.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,6 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/cidades")
 public class CidadeController {
 
@@ -30,6 +36,30 @@ public class CidadeController {
 
         return ResponseEntity.ok().body(cidades);
     }
+
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/listarPaginada")
+    public ResponseEntity<Page<Cidade>> listar(
+            @PageableDefault(sort = "id") Pageable pageable
+    ){
+
+        Page<Cidade> cidades = cidadeService.listar(pageable);
+
+        return ResponseEntity.ok().body(cidades);
+    }
+
+
+    @GetMapping("/estado/{idEstado}")
+    public ResponseEntity<List<Cidade>> buscarPorEstado(
+            @PathVariable Long idEstado
+    ){
+
+        List<Cidade> cidades = cidadeService.filtrarPorEstado(idEstado);
+        return ResponseEntity.ok().body(cidades);
+
+    }
+
 
     @PostMapping
     public ResponseEntity<Cidade> inserir(
