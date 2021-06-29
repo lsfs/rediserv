011ecc561180f55aed3871d613d97ef9cc126a19
Luís Fernando <lufesampaio@gmail.com>
2021-06-28 22:47:51 -0400

    feat: implementação da transação jpql

--- src/main/java/me/lsfs/rediserv/repositories/UnidadeRepository.java
+++ src/main/java/me/lsfs/rediserv/repositories/UnidadeRepository.java
@@ -1,6 +1,9 @@
 package me.lsfs.rediserv.repositories;
 
+import me.lsfs.rediserv.models.Cidade;
 import me.lsfs.rediserv.models.Unidade;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +15,10 @@ public interface UnidadeRepository extends JpaRepository<Unidade, Long> {
 
     @Query("SELECT u from Unidade u WHERE u.instituicao.id = :id")
     List<Unidade> findByInstituicao(Long id);
+
+    @Query("SELECT DISTINCT u FROM Unidade u")
+    Page<Unidade> listar(Pageable pageable);
+
+
+
 }
