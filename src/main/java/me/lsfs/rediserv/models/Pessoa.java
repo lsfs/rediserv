a03f4e33b7ea418d5d22a99f3ad0a76e269e4191
Luís Fernando <lufesampaio@gmail.com>
2021-06-28 22:50:32 -0400

    feat : criação do modelo de Pessoa

--- src/main/java/me/lsfs/rediserv/models/Pessoa.java
+++ src/main/java/me/lsfs/rediserv/models/Pessoa.java
@@ -1,9 +1,19 @@
 package me.lsfs.rediserv.models;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import javax.persistence.*;
+import java.util.*;
 
 @Entity
 @Table(name="pessoa")
-public class Pessoa {
+public class Pessoa implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,7 +21,16 @@ public class Pessoa {
 
     private String nome;
 
-   // private Usuario usuario;
+    private String email;
+
+    private String senha;
+
+    private boolean isPessoaAtiva;
+
+    private boolean bloqueada;
+
+    @ManyToMany(mappedBy = "pessoas", fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name = "idcargo")
@@ -21,5 +40,115 @@ public class Pessoa {
     @JoinColumn(name = "idunidade")
     private Unidade unidade;
 
+
+    public Pessoa() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public Unidade getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(Unidade unidade) {
+        this.unidade = unidade;
+    }
+
+    public boolean isPessoaAtiva() {
+        return isPessoaAtiva;
+    }
+
+    public void setPessoaAtiva(boolean pessoaAtiva) {
+        isPessoaAtiva = pessoaAtiva;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = this.roles;
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role perfil : roles){
+            authorities.add(new SimpleGrantedAuthority(perfil.getNome()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !bloqueada;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isPessoaAtiva;
+    }
+
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
 
 }
